4b8aa202a30caaaa56711f070d3119cb60f66008
I Made Eka Dwipayana <ekadwipayana99@gmail.com>
2026-03-17 03:49:29 -0400

    Update myapisqlouth.py

--- OuthPractice/myapisqlouth.py
+++ OuthPractice/myapisqlouth.py
@@ -130,12 +130,68 @@ def get_current_user(token:str = Depends(oauth2_scheme), db:Session = Depends(ge
             detail = "User does not exist",
             headers = {"WWW-Authenticate":"Bearer"}
         )
+    return user
+
+def get_current_active_user(current_user:User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Inactive User"
+        )
+    return current_user
+
+# Auth Endpoints
+@app.post("/register", response_model=UserResponse)
+def register_user(user: UserCreate,  db:Session = Depends(get_db)):
+    if db.query(User).filter(User.email == user.email).first():
+        raise HTTPException(
+            status_code = 404,
+            detail = "User already created"
+        )
+    
+    hashed_password= get_pwd_hash(user.password)
+    db_user = User(
+        name = user.name,
+        email = user.email,
+        role = user.role,
+        hashed_pwd = hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.post("/token", response_model=Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user= db.query(User).filter(User.email == form_data.username).first()
+
+    if not user or not verify_pwd(form_data.password, user.hashed_pwd):
+        raise HTTPException(
+            status_code = 404,
+            detail = "Wrong Information!"
+        )
+    
+    if not user.is_active:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Inactive User"
+        )
+    
+    access_token_expires = timedelta(minutes=TOKEN_EXPIRES)
+    access_token  = create_access_token(
+        data={"sub":user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type":"bearer"}
 
 
 # Endpoints (www.zerotoknowing.com/)
 @app.get("/")
 def root():
     return {"message":"Intro to Fast API with SQL"}
+
+@app.get("/profile", response_model = UserResponse)
+def get_profile(current_user:User = Depends(get_current_active_user)):
+    return current_user
 
 @app.get("/users/{user_id}",response_model=UserResponse)
 def get_user(user_id:int, db:Session = Depends(get_db)):
